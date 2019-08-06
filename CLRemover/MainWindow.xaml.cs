a16fdd393e717631beaa1044aa90798ec8cd94e3
AlexK <alkremn@gmail.com>
2019-08-06 13:47:33 -0600

    Add project files.

---
+++ CLRemover/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace CLRemover
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            var fullPath = path + "\\ZYTO5\\CloudLicense";
+            File.Delete(fullPath);
+            //label.Visibility = Visibility.Visible;
+        }
+    }
+}
