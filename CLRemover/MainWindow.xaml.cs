33a84de806ad86278e398b871242ca7d435a8d52
AlexK <alkremn@gmail.com>
2019-08-06 15:26:33 -0600

    Added aditional labels

--- CLRemover/MainWindow.xaml.cs
+++ CLRemover/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace CLRemover
             var fullPath = path + "\\ZYTO5\\CloudLicense";
             File.Delete(fullPath);
             zytoProLabel.Visibility = Visibility.Visible;
+            linkLabel.Visibility = Visibility.Visible;
         }
 
         private void Compass_Delete_Click(object sender, RoutedEventArgs e)
@@ -28,6 +29,12 @@ namespace CLRemover
             var fullPath = path + "\\ZYTOCompass2\\CloudLicense";
             File.Delete(fullPath);
             ZytoCompasLabel.Visibility = Visibility.Visible;
+            linkLabel.Visibility = Visibility.Visible;
+        }
+
+        private void Url_Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://zyto.auth0.com/login?state=g6Fo2SAwRG95aEh6Zm9nRnNyY1lTV20tbVVubldUZWFqbmkxRqN0aWTZIEc0XzRYQjBydi1VVVRKQjUyUUp2eVZLakIxSWIyUlRVo2NpZNkgd0xla1ZTV1FMaThvT0tySDVHMXFPTENNbFlHTUIxVDY&client=wLekVSWQLi8oOKrH5G1qOLCMlYGMB1T6&protocol=oauth2&response_type=code&redirect_uri=https%3A%2F%2Faccount.zyto.com%2Fsignin-auth0&scope=openid");
         }
     }
 }
