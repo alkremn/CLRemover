656cada3f7f0a694f2e7ce9ae74b0b629d7e1103
AlexK <alkremn@gmail.com>
2019-08-06 16:09:04 -0600

    Fixed the issue with the link

--- CLRemover/MainWindow.xaml.cs
+++ CLRemover/MainWindow.xaml.cs
@@ -34,7 +34,7 @@ namespace CLRemover
 
         private void Url_Button_Click(object sender, RoutedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://zyto.auth0.com/login?state=g6Fo2SAwRG95aEh6Zm9nRnNyY1lTV20tbVVubldUZWFqbmkxRqN0aWTZIEc0XzRYQjBydi1VVVRKQjUyUUp2eVZLakIxSWIyUlRVo2NpZNkgd0xla1ZTV1FMaThvT0tySDVHMXFPTENNbFlHTUIxVDY&client=wLekVSWQLi8oOKrH5G1qOLCMlYGMB1T6&protocol=oauth2&response_type=code&redirect_uri=https%3A%2F%2Faccount.zyto.com%2Fsignin-auth0&scope=openid");
+            System.Diagnostics.Process.Start("https://account.zyto.com/portal/accounts");
         }
     }
 }
